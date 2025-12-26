525232e60d924b7110f8b8a6cd3147a321e4431a
chenxing-dev <dev.chenxing@gmail.com>
2025-12-26 16:19:59 +0800

    style(Layout): adjust text size for better responsiveness

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,9 @@ type Props = React.PropsWithChildren<{ className?: string }>;
 
 export function Layout({ children, className }: Props) {
   return (
-    <div className={`${zpix.className} min-h-screen flex items-center justify-center`}>
+    <div
+      className={`${zpix.className} min-h-screen flex items-center justify-center text-lg 2xl:text-2xl`}
+    >
       <div className={`w-full max-w-2xl p-6 ${className ?? ""}`}>{children}</div>
     </div>
   );
