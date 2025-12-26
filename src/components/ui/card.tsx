4dad1001bcdca42ea3432df5fde0c613136b5d13
chenxing-dev <dev.chenxing@gmail.com>
2025-12-26 16:20:36 +0800

    style(card): adjust padding in Card component and update hr styles for better spacing

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-none border-[2px] border-card-foreground/80 py-6",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-none border-[2px] border-card-foreground/80 py-4 2xl:py-6",
         className
       )}
       {...props}
